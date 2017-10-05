2559560131ee1b7f16d17a9ff65e182b40cc81e8
Theodoros Liokos <theodoros.liokos@spryker.com>
2017-10-05 13:32:33 +0000

    ECO-499: Codeception Set Up

---
+++ tests/SprykerTest/Zed/Econda/Business/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SprykerTest\Zed\Econda\Business;
+
+use Codeception\Test\Unit;
+
+class ExampleTest extends Unit
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
