aada8937947c61e5e04ed2819759e5ee711c533b
Theodoros Liokos <theodoros.liokos@spryker.com>
2017-10-09 14:38:54 +0200

    ECO-857: Added configuration sample file to module

---
+++ config/config.dist.php
@@ -0,0 +1,7 @@
+<?php
+
+use SprykerEco\Shared\Econda\EcondaConstants;
+
+$config[EcondaConstants::ECONDA_CSV_FOLDER_PATH] = '';
+$config[EcondaConstants::ECONDA_CONFIG_BASIC_AUTH_USERNAME] = '';
+$config[EcondaConstants::ECONDA_CONFIG_BASIC_AUTH_PASSWORD] = '';
