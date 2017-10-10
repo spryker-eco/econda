550a2c70664de6bf0d44bf44bd11b25e869f83d0
Theodoros Liokos <theodoros.liokos@spryker.com>
2017-10-10 14:19:36 +0200

    ECO-857: Removed Unused Constants

--- config/config.dist.php
+++ config/config.dist.php
@@ -7,5 +7,3 @@
 use SprykerEco\Shared\Econda\EcondaConstants;
 
 $config[EcondaConstants::ECONDA_CSV_FOLDER_PATH] = '';
-$config[EcondaConstants::ECONDA_CONFIG_BASIC_AUTH_USERNAME] = '';
-$config[EcondaConstants::ECONDA_CONFIG_BASIC_AUTH_PASSWORD] = '';
