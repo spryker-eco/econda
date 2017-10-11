12e63dc00f083c397d39b625df5dcacbeee5f349
Theodoros Liokos <theodoros.liokos@spryker.com>
2017-10-11 18:05:37 +0200

    ECO-505: Debugging Environmental Variables

--- build/travis.sh
+++ build/travis.sh
@@ -1,72 +1,70 @@
 #!/usr/bin/env bash
-moduleName=$MODULE_NAME
-modulePath="$TRAVIS_BUILD_DIR/$MODULE_FOLDER"
-shopPath="$TRAVIS_BUILD_DIR/$SHOP_FOLDER"
-globalResult=1
-message=""
+
+buildResult=1
+buildMessage=""
 
 function runTests {
-    echo "define('APPLICATION_ROOT_DIR', '$shopPath');" >> "$shopPath/vendor/composer/autoload_real.php"
+    echo "define('APPLICATION_ROOT_DIR', '$TRAVIS_BUILD_DIR/$SHOP_FOLDER');" >> "$TRAVIS_BUILD_DIR/$SHOP_FOLDER/vendor/composer/autoload_real.php"
     echo "Running tests..."
-    cd "vendor/spryker-eco/$moduleName/"
-    "$shopPath/vendor/bin/codecept" run
+    cd "vendor/spryker-eco/$MODULE_NAME/"
+    "$TRAVIS_BUILD_DIR/$SHOP_FOLDER/vendor/bin/codecept" run
     if [ "$?" = 0 ]; then
         newMessage=$'\nTests are green'
-        message="$message$newMessage"
+        buildMessage="$buildMessage$newMessage"
         testResult=0
     else
         newMessage=$'\nTests are failing'
-        message="$message$newMessage"
+        buildMessage="$buildMessage$newMessage"
         testResult=1
     fi
-    cd "$shopPath"
+    cd "$TRAVIS_BUILD_DIR/$SHOP_FOLDER"
     echo "Done tests"
     return $testResult
 }
 
 function checkWithLatestDemoShop {
     echo "Checking module with latest DemoShop"
-    composer config repositories.ecomodule path $modulePath
-    composer require "spryker-eco/$moduleName @dev"
+    composer config repositories.ecomodule path "$TRAVIS_BUILD_DIR/$MODULE_FOLDER"
+    composer require "spryker-eco/$MODULE_NAME @dev"
     result=$?
     if [ "$result" = 0 ]; then
         newMessage=$'\nCurrent version of module is COMPATIBLE with latest DemoShop modules\' versions'
-        message="$message$newMessage"
+        buildMessage="$buildMessage$newMessage"
         if runTests; then
-            globalResult=0
+            buildResult=0
             checkModuleWithLatestVersionOfDemoShop
         fi
     else
         newMessage=$'\nCurrent version of module is NOT COMPATIBLE with latest DemoShop due to modules\' versions'
-        message="$message$newMessage"
+        buildMessage="$buildMessage$newMessage"
         checkModuleWithLatestVersionOfDemoShop
     fi
 }
 
 function checkModuleWithLatestVersionOfDemoShop {
     echo "Merging composer.json dependencies..."
-    updates=`php "$modulePath/build/merge-composer.php" "$modulePath/composer.json" composer.json "$modulePath/composer.json"`
+    updates=`php "$TRAVIS_BUILD_DIR/$MODULE_FOLDER/build/merge-composer.php" "$TRAVIS_BUILD_DIR/$MODULE_FOLDER/composer.json" composer.json "$TRAVIS_BUILD_DIR/$MODULE_FOLDER/composer.json"`
     if [ "$updates" = "" ]; then
         newMessage=$'\nModule is COMPATIBLE with latest versions of modules used in DemoShop'
-        message="$message$newMessage"
+        buildMessage="$buildMessage$newMessage"
         return
     fi
     newMessage=$'\nUpdated dependencies in module to match DemoShop\n'
-    message="$message$newMessage$updates"
+    buildMessage="$buildMessage$newMessage$updates"
     echo "Installing module with updated dependencies..."
-    composer require "spryker-eco/$moduleName @dev"
+    composer require "spryker-eco/$MODULE_NAME @dev"
     result=$?
     if [ "$result" = 0 ]; then
         newMessage=$'\nModule is COMPATIBLE with latest versions of modules used in DemoShop'
-        message="$message$newMessage"
+        buildMessage="$buildMessage$newMessage"
         runTests
     else
         newMessage=$'\nModule is NOT COMPATIBLE with latest versions of modules used in DemoShop'
-        message="$message$newMessage"
+        buildMessage="$buildMessage$newMessage"
     fi
 }
 
 cd $SHOP_FOLDER
 checkWithLatestDemoShop
-echo "$message"
-exit $globalResult
+echo "$buildMessage"
+exit $buildResult
