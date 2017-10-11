75b1720d8fa4823e8486fc43d15b3cbb4dc66abd
Theodoros Liokos <theodoros.liokos@spryker.com>
2017-10-11 14:09:06 +0000

    ECO-505: Defined env variables for foldera

--- move-module.sh
+++ move-module.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 echo "Trying to move the module to a subfolder..."
-mkdir module
-mv * module/
-result=$?
-if [ "$result" = 0 ]; then
+mkdir $BUILD_MODULE_FOLDER && ls -1 | grep -v ^$BUILD_MODULE_FOLDER | xargs -I{} mv {} $BUILD_MODULE_FOLDER
+if [ "$?" = 0 ]; then
     echo 'Module successfully moved to subfolder'
 fi
 echo 'Failed to move the module to a subfolder'
