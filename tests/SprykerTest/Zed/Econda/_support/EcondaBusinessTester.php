0c78603d822d1125faaf0637e1e7e78d9839533d
Theodoros Liokos <theodoros.liokos@spryker.com>
2017-10-18 15:07:16 +0200

    ECO-498: Changed composer.json

--- tests/SprykerTest/Zed/Econda/_support/EcondaBusinessTester.php
+++ tests/SprykerTest/Zed/Econda/_support/EcondaBusinessTester.php
@@ -1,6 +1,8 @@
 <?php
 namespace SprykerTest\Zed\Econda;
 
+use Codeception\Actor;
+
 /**
  * Inherited Methods
  * @method void wantToTest($text)
@@ -15,8 +17,8 @@ namespace SprykerTest\Zed\Econda;
  * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
  *
  * @SuppressWarnings(PHPMD)
-*/
-class EcondaBusinessTester extends \Codeception\Actor
+ */
+class EcondaBusinessTester extends Actor
 {
     use _generated\EcondaBusinessTesterActions;
 
